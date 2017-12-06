c494e002e2002b4a2bb7237836c457b5a5fc4fbd
hanlin <hanlaung@stanford.edu>
2017-12-05 17:45:56 -0800

    Made some changes

--- components/editableProfile/editableProfileController.js
+++ components/editableProfile/editableProfileController.js
@@ -1,6 +1,11 @@
 'use strict';
 
 /* Home view controller. Adjust welcome message and toolbar title */
-cs50App.controller('privateProfileController', ['$scope','$routeParams','$resource',
-function ($scope, $rootScope, $routeParams, $resource) {}
-]);
+cs50App.controller('privateProfileController', ['$scope', '$rootScope', '$routeParams','$resource',
+function ($scope, $rootScope, $routeParams, $resource) {
+  var resource = $resource("/user/" + $scope.main.login_name);
+
+  resource.get({}, function(model){
+      console.log(model)
+  });
+}]);
